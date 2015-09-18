5794867dacb326b16136f01a3285ea70a4db5a3b
James Roland <james.a.roland@gmail.com>
2015-09-17 22:07:22 -0700

    Finished formatting of request

---
+++ Photorama/FlickerAPI.swift
@@ -0,0 +1,54 @@
+//
+//  FlickerAPI.swift
+//  Photorama
+//
+//  Created by James Roland on 9/17/15.
+//  Copyright (c) 2015 LinkedIn. All rights reserved.
+//
+
+import Foundation
+
+enum Method: String {
+    case RecentPhotos = "flickr.photos.getRecent"
+}
+
+private let baseURLString = "https://api.flickr.com/services/rest"
+private let APIKey = "a6d819499131071f158fd740860a5a88"
+
+class FlickerAPI {
+    
+    private class func flickrUrl(#method: Method, parameters: [String:String]?) -> NSURL? {
+        if let components = NSURLComponents(string: baseURLString) {
+            var queryItems = [NSURLQueryItem]()
+            var baseParams = [
+                "method": method.rawValue,
+                "format": "json",
+                "nojsoncallback": "1",
+                "api_key": APIKey
+            ]
+            
+            for (key, value) in baseParams {
+                let item = NSURLQueryItem(name: key, value: value)
+                queryItems.append(item)
+            }
+            
+            if let additionalParams = parameters {
+                for (key, value) in additionalParams {
+                    let item = NSURLQueryItem(name: key, value: value)
+                    queryItems.append(item)
+                }
+            }
+            components.queryItems = queryItems
+            return components.URL
+        }
+        
+        return nil
+    }
+    
+    
+    class func recentPhotosURL() -> NSURL? {
+        return flickrUrl(method: .RecentPhotos, parameters: ["extras": "url_h,date_taken"])
+    }
+    
+    
+}
