64d1a556275a97160863643b32cac63a2edf44a6
James Roland <james.a.roland@gmail.com>
2015-09-18 08:10:15 -0700

    Convert from JSON object to Photo Object

--- Photorama/FlickerAPI.swift
+++ Photorama/FlickerAPI.swift
@@ -20,7 +20,48 @@ enum PhotosResult {
 private let baseURLString = "https://api.flickr.com/services/rest"
 private let APIKey = "a6d819499131071f158fd740860a5a88"
 
+private let dateFormatter: NSDateFormatter = {
+    let formatter = NSDateFormatter()
+    formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+    return formatter
+}()
+
+func createError(localizedDescription: String) -> NSError {
+    return NSError(domain: "com.bignerdranch.photorama",
+        code: 0,
+        userInfo: [NSLocalizedDescriptionKey : localizedDescription])
+}
+
 class FlickerAPI {
+    
+    class func recentPhotosURL() -> NSURL? {
+        return flickrUrl(method: .RecentPhotos, parameters: ["extras": "url_h,date_taken"])
+    }
+    
+    class func photosFromJSONData(data: NSData) -> PhotosResult {
+        var jsonParsingError: NSError?
+        if let jsonObject: AnyObject = NSJSONSerialization.JSONObjectWithData(data,
+            options: nil,
+            error: &jsonParsingError) {
+                
+                if let photos = jsonObject["photos"] as? [String : AnyObject] {
+                    if let photosArray = photos["photo"] as? [[String : AnyObject]] {
+                        for photoJSON in photosArray {
+                            println("Photo: \(photoJSON)")
+                        }
+                    }
+                }
+                
+                // For now, just return an empty array
+                return .Success([Photo]())
+        }
+        else {
+            println("Error parsing JSON: \(jsonParsingError!)")
+            return .Failure(jsonParsingError!)
+        }
+    }
+    
+    //MARK: Private methods.
     
     private class func flickrUrl(#method: Method, parameters: [String:String]?) -> NSURL? {
         if let components = NSURLComponents(string: baseURLString) {
@@ -50,32 +91,25 @@ class FlickerAPI {
         return nil
     }
     
-    
-    class func recentPhotosURL() -> NSURL? {
-        return flickrUrl(method: .RecentPhotos, parameters: ["extras": "url_h,date_taken"])
-    }
-    
-    class func photosFromJSONData(data: NSData) -> PhotosResult {
-        var jsonParsingError: NSError?
-        if let jsonObject: AnyObject = NSJSONSerialization.JSONObjectWithData(data,
-            options: nil,
-            error: &jsonParsingError) {
-                
-                if let photos = jsonObject["photos"] as? [String : AnyObject] {
-                    if let photosArray = photos["photo"] as? [[String : AnyObject]] {
-                        for photoJSON in photosArray {
-                            println("Photo: \(photoJSON)")
-                        }
-                    }
-                }
-                
-                // For now, just return an empty array
-                return .Success([Photo]())
+    private class func photoFromJSONObject(json: [String : AnyObject]) -> Photo? {
+        var photoID = json["id"] as? String
+        var title = json["title"] as? String
+        var url: NSURL?
+        
+        if let photoURLString = json["url_h"] as? String {
+            url = NSURL(string: photoURLString)
         }
-        else {
-            println("Error parsing JSON: \(jsonParsingError!)")
-            return .Failure(jsonParsingError!)
+        
+        var dateTaken: NSDate?
+        if let dateString = json["datetaken"] as? String {
+            dateTaken = dateFormatter.dateFromString(dateString)
         }
+        
+        if title == nil || photoID == nil || url == nil || dateTaken == nil {
+            return nil
+        }
+        
+        return Photo(title: title!, photoID: photoID!, URL: url!, dateTaken: dateTaken!)
     }
     
     
