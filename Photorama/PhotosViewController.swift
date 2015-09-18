730a0911a4923662a75b409632f9532f2890a592
James Roland <james.a.roland@gmail.com>
2015-09-17 21:57:18 -0700

    Added UIImageView

---
+++ Photorama/PhotosViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by James Roland on 9/17/15.
+//  Copyright (c) 2015 LinkedIn. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
