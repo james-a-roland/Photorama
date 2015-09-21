5a8cb8cb4729e1e264c9591c612b5fea2653256f
James Roland <james.a.roland@gmail.com>
2015-09-21 08:30:13 -0700

    ImageView connections are working

---
+++ Photorama/PhotoCollectionViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Photorama
+//
+//  Created by James Roland on 9/21/15.
+//  Copyright (c) 2015 LinkedIn. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
+    func updateWithImage(image: UIImage?) {
+        if let imageToDisplay = image {
+            spinner.stopAnimating()
+            imageView.image = imageToDisplay
+        }
+        else {
+            spinner.startAnimating()
+            imageView.image = nil
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        updateWithImage(nil)
+    }
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        updateWithImage(nil)
+    }
+}
